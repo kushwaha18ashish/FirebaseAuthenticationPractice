09fac2cc528d682a4b3955d8f152808cd033e57f
kushwaha18ashish <ashish725494@gmail.com>
2025-04-14 18:17:56 +0530

    second Commit@18:17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mjolnir.firebaseauthenticationpractice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mjolnir.firebaseauthenticationpractice"
         minSdk = 22
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/mjolnir/firebaseauthenticationpractice/Login.kt
+++ app/src/main/java/com/mjolnir/firebaseauthenticationpractice/Login.kt
@@ -1,20 +1,29 @@
 package com.mjolnir.firebaseauthenticationpractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+    private lateinit var tvRegister:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        tvRegister=findViewById(R.id.tvRegister)
+
+        tvRegister.setOnClickListener {
+            val intent=Intent(this,Register::class.java)
+            startActivity(intent)
+            finish()
         }
+
+
+
     }
 }
